0d49a12aa9fb2817dcfec149f93a8172658f7e9a
Gabriel <103671129+MMXXIV@users.noreply.github.com>
2024-09-05 16:34:27 +0200

    limit adjust

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const API_URL_SPECIES = 'https://pokeapi.co/api/v2/pokemon-species';
 
 export const GIF_URL = 'https://pokexperto.net/3ds/sprites/pokedex/animados';
 
-export const LIMIT = 5;
+export const LIMIT = 20;
